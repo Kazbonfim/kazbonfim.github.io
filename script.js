ac785394a759fee83fc3c772b9b3659421ccb80a
Kazbonfim <78633256+Kazbonfim@users.noreply.github.com>
2022-12-10 11:48:09 -0300

    Pequenas mudanças no layout

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ avatar.addEventListener('mouseover', (a) => {
 });
 
 avatar.addEventListener('mouseout', (a) => {
-    avatar.src = './resources/git.png'
-    return
+    setTimeout(() => {
+        avatar.src = './resources/git.png'
+        return
+}
+, 350)
 });
 
 // Script para realizar a mudança na 'horizontalColumn' ao realizar o Scroll.
@@ -17,6 +20,7 @@ addEventListener('load', () => {
     body = document.body;
     html1 = doc.documentElement;
     addEventListener('scroll', () => {
-        document.getElementById('horizontalColumn').style.backgroundColor = html1.scrollTop > 90 ? 'whitesmoke' : '#474c68';
+        document.getElementById('horizontalColumn').style.backgroundColor = html1.scrollTop > 90 ? 'rgba(245, 245, 245, 0.9)' : '#474c68';
+        document.getElementById('footer').style.color = html1.scrollTop > 90 ? 'black' : 'whitesmoke';
     })
 })
