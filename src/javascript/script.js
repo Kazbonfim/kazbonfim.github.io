0d7f6350c3f73e75b2ada0ed582fdd62300e462b
Kazbonfim <zcry.games@gmail.com>
2024-09-25 00:59:27 -0300

    Novas melhorias feitas; avatar mais responsivo, e remoção de alguns elementos inúteis...

---
+++ src/javascript/script.js
@@ -0,0 +1,25 @@
+// Script para realizar a mudança na 'horizontalColumn' ao realizar o Scroll.
+let doc, body, html1
+addEventListener('load', () => {
+    doc = document;
+    body = document.body;
+    html1 = doc.documentElement;
+    addEventListener('scroll', () => {
+        document.querySelector('body').style.backgroundColor = html1.scrollTop > 420 ? '#19191A' : '#0E6CC4';
+    })
+});
+
+// Script para alterar a cor de fundo, conforme o scroll da página.
+let doc1, head, html2
+    addEventListener('load', () => {
+        doc = document;
+        head = document.head
+        html2 = doc.documentElement
+        addEventListener('scroll', () => {
+            document.getElementById('waveSelector').href = html2.scrollTop > 420 ? '/src/style/blackwave.css' : '/src/style/wave.css';
+        })
+    })
+
+
+
+
